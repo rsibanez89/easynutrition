557bdd9bfed2bac77c7e127bedc72f61e9d671e9
Rodrigo Ibañez <rsibanez89@gmail.com>
2015-05-18 00:33:32 -0300

    Dejo inestable, no se que paso pero no anda nada... :S

--- src/main/java/com/easynutrition/entity/Evaluation.java
+++ src/main/java/com/easynutrition/entity/Evaluation.java
@@ -7,17 +7,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.easynutrition.api.rest.serializer.ApiRestSerializerCalendar;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Entity
 @Table(name = "evaluation")
