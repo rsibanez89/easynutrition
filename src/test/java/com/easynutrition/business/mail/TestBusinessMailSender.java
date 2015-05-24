fd2a57c17cb0e73504efca18ea58acf344fe2a5b
Juan Feldman <juanfeldman11@gmail.com>
2015-05-24 19:22:50 -0300

    Agrego caso de test para envio de mail

---
+++ src/test/java/com/easynutrition/business/mail/TestBusinessMailSender.java
@@ -0,0 +1,63 @@
+package com.easynutrition.business.mail;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath*:META-INF/spring/*-context.xml")
+@WebAppConfiguration
+public class TestBusinessMailSender {
+	@Autowired
+	private BusinessMailSender mailSender;
+	@Autowired
+	private ThreadPoolTaskExecutor mailExecutor;
+
+	
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		// Sets JNDI Mock for Spring Security
+		// This does not work for Hibernate, so we override persistence.xml
+		SimpleNamingContextBuilder builder = SimpleNamingContextBuilder.emptyActivatedContextBuilder();
+	    
+	    DriverManagerDataSource ds = new DriverManagerDataSource();
+	    ds.setUrl("jdbc:mysql://localhost:3306/easynutrition");
+	    ds.setUsername("root");
+	    ds.setPassword("root");
+	    
+	    builder.bind("java:comp/env/jdbc/EasyNutritionDS", ds);
+	}
+	
+	@Before
+	public void setUp() {
+		mailExecutor.setWaitForTasksToCompleteOnShutdown(true);
+	}
+
+	@Test
+	public void testSendMailNewPatient() throws Exception {
+		final String[] mails = {};
+		
+		for (String mail : mails) {
+			mailSender.sendMailNewPatient(mail, "username", "password", "nutricionist", new Locale("es"));
+		}
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		mailExecutor.shutdown();
+		mailExecutor.getThreadPoolExecutor().awaitTermination(1, TimeUnit.DAYS);
+	}
+	
+}
